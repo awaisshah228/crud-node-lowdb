ffe51f2ff8fea036c54d7115d2137f2df88ce02b
awaisshah228 <m.awaisshah228@gmail.com>
2022-12-22 19:03:40 +0500

    done

--- app.js
+++ app.js
@@ -22,19 +22,4 @@ db.data ||= { notes: [] };
 
 app.use('/',noteRouter)
 
-// // get all notes
-// app.get("/", getNotes);
-
-// // get notes by id
-// app.get("/note/:id", getSingleNote);
-
-// // create notes
-// app.post("/note", createNote);
-
-// // replace post
-// app.put("/note/:id", updateNote);
-
-// // delete post
-// app.delete("/note/:id", deleteNote);
-
 export { app, db };
